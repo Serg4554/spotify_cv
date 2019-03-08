435ce41efc65db6199f63515a8e363cfafe050a1
Serg <Sergio.357.95@gmail.com>
2019-03-08 21:52:39 +0100

    Added enabled attribute

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -2,21 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from './button.module.css';
 
-const Button = (props) => (
-  <div {...props}>
+const Button = (props) => {
+  const {size, enabled, onClick, ...other} = props;
+
+  let classNames = [style.button, style[props.size]];
+  if(!props.enabled) {
+    classNames.push(style.disabled);
+  }
+
+  return (
     <div
-      className={[style.button, style[props.size]].join(' ')}>
-      {props.children}
+      {...other}
+      onClick={() => {
+        if(props.enabled && onClick) {
+          onClick();
+        }
+      }}
+    >
+      <div className={classNames.join(' ')}>{props.children}</div>
     </div>
-  </div>
-);
+  );
+};
 
 Button.propTypes = {
-  size: PropTypes.oneOf(['small', 'medium', 'large'])
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  enabled: PropTypes.bool
 };
 
 Button.defaultProps = {
   size: 'medium',
+  enabled: true
 };
 
 export default Button;
