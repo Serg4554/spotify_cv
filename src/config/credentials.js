3a3432df07c16e94d0bca319111340137136f82c
Serg <Sergio.357.95@gmail.com>
2019-03-13 14:00:48 +0100

    Use the current url instead of localhost

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,10 +1,11 @@
+const url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
 
 export default {
   playerName: "This is me. Serg",
   getAuthUri: (uuid) => "https://accounts.spotify.com/authorize" +
     "?client_id=7fd1e1e916ad43679ae2a30ddd43e728" +
     "&response_type=token" +
-    "&redirect_uri=" + encodeURI("http://localhost:3000" + "/welcome") +
+    "&redirect_uri=" + encodeURI(url + "/welcome") +
     "&state=" + uuid +
     "&scope=" + encodeURI("streaming user-read-birthdate user-read-email user-read-private user-read-playback-state user-modify-playback-state")
 }
